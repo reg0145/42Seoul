662185faa1b212da3be1eb9c23fa242cf164c53f
reg0145 <reg0145@naver.com>
2021-09-15 00:44:53 +0900

    ft_strcapitalize.c

--- c_02/ex09/ft_strcapitalize.c
+++ c_02/ex09/ft_strcapitalize.c
@@ -6,38 +6,47 @@
 /*   By: donghyuk <donghyuk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 23:26:46 by donghyuk          #+#    #+#             */
-/*   Updated: 2021/09/14 23:46:00 by donghyuk         ###   ########.fr       */
+/*   Updated: 2021/09/15 00:43:42 by donghyuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	ft_char_is_lowercase(char c)
+char	*ft_char_is_lowercase(char *c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return c + ('a' - 'A');
-	return c;
+	if (*c >= 'A' && *c <= 'Z')
+		*c += ('a' - 'A');
+	return (c);
 }
 
-char	ft_char_is_uppercase(char c)
+char	*ft_char_is_uppercase(char *c)
 {
-	if (c >= 'a' && c <= 'z')
-		return c - ('a' - 'A');
-	return c;
+	if (*c >= 'a' && *c <= 'z')
+		*c -= ('a' - 'A');
+	return (c);
 }
 
 char	*ft_strcapitalize(char *str)
 {
 	int	is_new_word;
-	int	index;
-	
-	index = 0;
+	int	i;
+
+	i = 0;
 	is_new_word = 1;
-	while (str[index] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[index] == ' ')
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if (is_new_word)
+			{
+				ft_char_is_uppercase(&str[i]);
+				printf("%c!", str[i]);
+				is_new_word = 0;
+			}
+			else
+				ft_char_is_lowercase(&str[i]);
+		}
+		else if ((str[i] < '0' || str[i] > '9'))
 			is_new_word = 1;
-		else if (is_new_word)
-			str[index] = ft_char_is_uppercase(str[index]);
-		else
-			str[index] = ft_char_is_lowercase(str[index]);
+		i++;
 	}
+	return (str);
 }
