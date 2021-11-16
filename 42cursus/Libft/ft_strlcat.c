37bcb65acbf89d6c201a574057406d55b60b0ac3
reg0145 <reg0145@naver.com>
2021-11-16 21:08:45 +0900

    42cursus

---
+++ 42cursus/Libft/ft_strlcat.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donghyuk <donghyuk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 16:08:05 by donghyuk          #+#    #+#             */
+/*   Updated: 2021/11/16 17:22:44 by donghyuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	while (*s != 0)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (0);
+}
