e6fb2ab7eec4209922653dc579738db2ffa278bd
Donghyun Kim <donghyuk@c3r5s3.42seoul.kr>
2022-08-06 19:47:55 +0900

    push_swap

--- 42cursus/pushswap/checker_bonus.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHECKER_BONUS_H
-# define CHECKER_BONUS_H
-
-# include "get_next_line_bonus.h"
-
-#endif
--- 42cursus/pushswap/error_bonus.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERROR_BONUS_H
-# define ERROR_BONUS_H
-
-void	ft_error(char *msg);
-
-#endif
