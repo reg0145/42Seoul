da7d6f23ef4721b8448a0641c9b469f0ed04c166
donghyuk <reg0145@naver.com>
2023-03-10 08:04:49 +0900

    [Fix] wordpress shell코드 간략화

--- 42cursus/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ 42cursus/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#work dir : /var/www/html
-
 db_name=`wp config get DB_NAME 2> /dev/null`
 wp_admin=`wp user get admin --field=user_login 2> /dev/null`
 wp_user=`wp user get ${WP_USER} --field=user_login 2> /dev/null`
@@ -9,11 +7,12 @@ wp_user=`wp user get ${WP_USER} --field=user_login 2> /dev/null`
 if [ ! -f "/var/www/html/index.php" ]; then
 	wp core download --locale=ko_KR --path=/var/www/html --force
 fi
+
 if [ "$db_name" = "" ]; then
 	wp config create --dbhost=mariadb:3306 --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_USER_PASSWORD}
 fi
 
-if ["$wp_admin" = ""] || [ "$wp_admin" = 'admin' ]; then
+if [ "$wp_admin" = "" ] || [ "$wp_admin" = 'admin' ]; then
 	wp core install --admin_user=${WP_ADMIN} --admin_email=${WP_ADMIN_EMAIL} --admin_password=${WP_ADMIN_PASSWORD} --url=http://donghyuk.42.fr --title='inception'
 fi
 
